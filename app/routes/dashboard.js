30adb22e930562835d0b153f17cb5b1d76377a6e
chrisbodhi <cboette@gmail.com>
2015-08-06 02:07:55 +0000

    Render placeholder JSON data in dashboard view.

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return Ember.$.getJSON('data.json').then(function(data) {
+      return data;
+    });
+  }
 });
